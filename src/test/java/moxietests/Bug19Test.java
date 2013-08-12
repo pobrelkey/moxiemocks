c830650c1b856fcaeab7fd706cfc1b6871d41762
rob-googlecode@pelkey.org <rob-googlecode@pelkey.org@294e9413-25e5-7e50-c874-a25f1dd29ab7>
2013-08-12 16:47:15 +0000

    fix bug 19 - default behavior of constructor mocks is now to return a new instance of the mocked class

---
+++ src/test/java/moxietests/Bug19Test.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2013 Moxie contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package moxietests;
+
+import moxie.Moxie;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PartiallyMocked.class)
+public class Bug19Test {
+
+    @Test
+    public void constructors_defaultBehaviorShouldBeToReturnANewInstance() {
+        Moxie.expect(PartiallyMocked.class).times(3).onNew(Moxie.startsWith("porridge"));
+        Moxie.expect(PartiallyMocked.class).times(2).onNew(Moxie.startsWith("dog"));
+
+        Assert.assertThat(new PartiallyMocked("porridge oats"), Matchers.instanceOf(PartiallyMocked.class));
+        Assert.assertThat(new PartiallyMocked("porridge-colored sky"), Matchers.instanceOf(PartiallyMocked.class));
+        Assert.assertThat(new PartiallyMocked("dog-eared"), Matchers.instanceOf(PartiallyMocked.class));
+        Assert.assertThat(new PartiallyMocked("dog's breakfast"), Matchers.instanceOf(PartiallyMocked.class));
+        Assert.assertThat(new PartiallyMocked("porridge grey"), Matchers.instanceOf(PartiallyMocked.class));
+    }
+
+}
